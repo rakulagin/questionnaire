35ca87ddb7b0ab86bb0b9b39335148c8b450a559
rakulagin <koulagin@gmail.com>
2022-09-07 23:16:50 +0400

    сделать нормальные кнопки далее
    доверстал форму (внешне) кроме кастомного селекта

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useForm } from "react-hook-form";
 
 import './Registration.scss'
@@ -15,46 +15,83 @@ const Registration = () => {
    return (
        <div className="mockup mockup__wrapper">
            <form onSubmit={handleSubmit(onSubmit)}>
-               <div>
-                   <label for="name" className="test1">Как вас зовут?</label>
-                   <input id="name" className="input-name" type="text" {...register("name", { required: true, minLength: 2 })}/>
-                   {errors.name && <span>error</span>}
+
+
+               <div className="input__block">
+                   <label for="name" className="input__question">Как вас зовут?</label>
+                   <input
+                       id="name"
+                       className="input__field input__answer"
+                       type="text"
+                       {...register("name", { required: true, minLength: 2 })}/>
+                   {errors.name && <span>Здесь ошибка</span>}
                </div>
 
-               <div>
-                   <h2 className="test1">Ваш пол</h2>
-                   <input type="radio" checked name="gender" id="man" value="male" {...register("gender")}/>
-                   <label for="man">Мужчина</label>
-                   <input type="radio" name="gender" id="woman" value="female" {...register("gender")}/>
-                   <label for="woman">Женщина</label>
+
+               <div className="input__block">
+                   <h2 className="input__question">Ваш пол</h2>
+                   <input
+                       className="input__radio"
+                       type="radio"
+                       // name="gender"
+                       id="man"
+                       value="male"
+                       {...register("gender")}
+                   />
+                   <label for="man" className="input__answer">Мужчина</label>
+
+                   <input
+                       className="input__radio"
+                       type="radio"
+                       // name="gender"
+                       id="woman"
+                       value="female"
+                       {...register("gender")}
+                   />
+                   <label for="woman" className="input__answer">Женщина</label>
                </div>
 
-               <div>
-                   <label for="name" className="test1">В каком году вы родились?</label>
-                   <input id="name" className="input-name" type="number" {...register("year")}/>
+               <div className="input__block">
+                   <label for="name" className="input__question">В каком году вы родились?</label>
+                   <input id="name" className="input__field input__answer" type="number" {...register("year")}/>
                </div>
 
-               <div>
-                   <h2 className="test1">Находитесь ли вы в отношениях?</h2>
-                   <input type="radio" checked name="relationship" id="yes" value="yes" {...register("relationship")}/>
-                   <label for="yes">Да</label>
-                   <input type="radio" name="relationship" id="no" value="no" {...register("relationship")}/>
-                   <label for="no">Нет</label>
+               <div className="input__block">
+                   <h2 className="input__question">Находитесь ли вы в отношениях?</h2>
+                   <input className="input__radio" type="radio" name="relationship" id="yes" value="yes" {...register("relationship")}/>
+                   <label for="yes" className="input__answer">Да</label>
+                   <input className="input__radio" type="radio" name="relationship" id="no" value="no" {...register("relationship")}/>
+                   <label for="no" className="input__answer">Нет</label>
                </div>
 
-               <div>
-                   <label for="city" className="test1">В каком городе вы живете?</label>
-                   <input id="city" className="input-name" type="text" {...register("city")}/>
+               <div className="input__block">
+                   <label for="city" className="input__question">В каком городе вы живете?</label>
+                   <input id="city" className="input__field input__answer" type="text" {...register("city")}/>
                </div>
 
-               <div>
-                   <label for="height" className="test1">Ваш рост?</label>
-                   <input id="height" className="input-name" type="number" {...register("height")}/>
+               <div className="input__block">
+                   <label for="height" className="input__question">Ваш рост?</label>
+                   <input id="height" className="input__field input__answer" type="number" {...register("height")}/>
                </div>
 
-               <div>
-                   <label for="weight" className="test1">Ваш вес?</label>
-                   <input id="weight" className="input-name" type="number" {...register("weight")}/>
+               <div className="input__block">
+                   <label for="weight" className="input__question">Ваш вес?</label>
+                   <input id="weight" className="input__field input__answer" type="number" {...register("weight")}/>
+               </div>
+
+               <div className="input__block">
+                   <h2 className="input__question">Какое у вас образование?</h2>
+                   <select
+                       className="input__field input__answer"
+                       {...register("education")}>
+                       <option value="1">Начальное</option>
+                       <option value="2">Среднее</option>
+                       <option value="3">Средне-специальное</option>
+                       <option value="4">Незаконченное высшее</option>
+                       <option value="5">Высшее</option>
+                       <option value="6">Несколько высших</option>
+                       <option value="7">Ученая степень</option>
+                   </select>
                </div>
 
                {/*<div>*/}
